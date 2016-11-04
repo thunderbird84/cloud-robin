a0b8afc1ead078973adefe1cbc0993947be548b3
thunderbird84 <robin.github@gmail.com>
2016-11-04 22:45:42 +0700

    add library

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,29 @@
-var main =1;
-alert('hello worlds')
+var JSObject= {
+			extend:function(props){
+				$this=function(args){					
+					if(typeof(args)=='object'){
+						for(var key in args){
+							 if (args.hasOwnProperty(key)) {
+								this[key]= args[key];
+							}
+						}
+					}else if(typeof(args)=='string'){
+						this.elm = document.getElementById(args);
+					}
+					if(typeof(this.init)=='function'){
+						this.init();
+					}
+				}				
+				_props =props||{};
+				_props.toString=function(){
+					if(this.type){
+						return this.type;
+
+					}else{
+						return 'JSObject';
+					}
+				};						
+				$this.prototype=_props||{};
+				return $this;
+			}				
+		}
