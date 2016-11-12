a01f46efd0f731f6634dfb70555574a14c9eef56
thunderbird84 <robin.github@gmail.com>
2016-11-12 14:48:39 +0700

    init robin-crypto

---
+++ deps/crypto/index.js
@@ -0,0 +1,76 @@
+ var java = require("java");
+java.classpath.push("tools-1.0-SNAPSHOT.jar");
+var Crypto= java.import('robin.tools.Crypto');
+var AES256CtrCrypto= java.import('robin.tools.AES256CtrCrypto');
+var ByteBuffer= java.import('java.nio.ByteBuffer');
+
+var fs = require('fs');
+
+var toJavaBuffer=function(buf){
+    if(buf instanceof Buffer){
+       var javaBuff = java.callStaticMethodSync("java.nio.ByteBuffer", "allocate",buf.length);      
+       for(var i =0; i< buf.length;i++){
+          javaBuff.putSync(java.newByte(buf.readInt8(i)))
+       }
+       return javaBuff;
+    }
+    return undefined;
+}
+var fromJavaBuffer=function(buf){
+  if(buf && buf.toString().lastIndexOf('java.nio.HeapByteBuffer[', 0) === 0){
+     var bytes = buf.arraySync();
+     var nodeBuff = new Buffer(bytes.length);     
+     for(var i =0; i< bytes.length;i++){
+       nodeBuff.writeInt8(bytes[i], i);
+     }  
+     return nodeBuff;
+  }
+  return undefined;
+}
+
+var gzipSync=function(buf){
+   var jbuff = java.callStaticMethodSync('robin.tools.Crypto', "gzip",toJavaBuffer(buf)); 
+   return fromJavaBuffer(jbuff);
+}
+
+var gunzipSync=function(buf){
+   var jbuff = java.callStaticMethodSync('robin.tools.Crypto', "gunzip",toJavaBuffer(buf)); 
+   return fromJavaBuffer(jbuff);
+}
+
+var hello =function(){  
+   console.log("hello");
+   
+}
+
+var zLibCompress = function(buf){
+   var jbuff = java.callStaticMethodSync('robin.tools.Crypto', "zLibCompress",toJavaBuffer(buf)); 
+   return fromJavaBuffer(jbuff);
+}
+
+var zLibDecompress = function(buf){
+   var jbuff = java.callStaticMethodSync('robin.tools.Crypto', "zLibDecompress",toJavaBuffer(buf)); 
+   return fromJavaBuffer(jbuff);
+}
+
+var RobinCrypto = function(password){
+  var _crypto =  new AES256CtrCrypto(password);
+  this.encrypt = function(raw){
+      var jbuff = _crypto.encryptSync(toJavaBuffer(raw));
+      return fromJavaBuffer(jbuff);
+  } ,
+  this.decrypt = function(encrypted){
+       var jbuff = _crypto.decryptSync(toJavaBuffer(encrypted));
+      return fromJavaBuffer(jbuff);
+  } 
+}
+
+exports.toJavaBuffer=toJavaBuffer;
+exports.fromJavaBuffer=fromJavaBuffer;
+exports.gzipSync=gzipSync;
+exports.gunzipSync=gunzipSync;
+exports.zLibCompress=zLibCompress;
+exports.zLibDecompress=zLibDecompress;
+exports.RobinCrypto = RobinCrypto;
+exports.unzipSync=zLibDecompress;
+exports.hello=hello;
---
+++ deps/crypto/test.js
@@ -0,0 +1,55 @@
+var rb= require('robin-crypto');
+var fs = require('fs');
+var Benchmark = require('benchmark');
+const assert = require('assert');
+
+
+var buf = new Buffer("TEST STRING THAT USE TO COMPRESS", 'utf8');
+console.log(buf.toString());
+var javaBuff = rb.toJavaBuffer(buf)
+console.log(typeof javaBuff)
+var nodeBuff = rb.fromJavaBuffer(javaBuff);
+console.log(nodeBuff.toString());
+assert.equal(buf.toString(),nodeBuff.toString());
+rb.hello();
+
+var rbCrypto = new rb.RobinCrypto('123412344gfgfgdg gdfgdfgfg dfggfgfgf fgfg');
+
+console.log(rbCrypto.decrypt(rbCrypto.encrypt(buf)).toString());
+var rand =function () {
+    n = Math.random()*1e17;
+    return (n+"").substr(1,10000);
+   }
+//process.exit();
+console.log("\nPERFORMANCE TEST\n-----------------------\n")
+var suite = new Benchmark.Suite; 
+suite.add('#toJavaBuffer', function() {
+   var javaBuff = rb.toJavaBuffer(buf)   
+})
+.add('#fromJavaBuffer', function() {
+   var nodeBuff = rb.fromJavaBuffer(javaBuff);  
+})
+.add('#RobinCrypto', function() {	
+
+   var buf = new Buffer(rand());
+   //console.log(buf.toString())
+   var test =rbCrypto.decrypt(rbCrypto.encrypt(buf));
+   assert.equal(buf.toString(),test.toString());
+})
+.add('#gzipSync', function() {
+  var test = rb.gunzipSync(rb.gzipSync(nodeBuff))
+  assert.equal(nodeBuff.toString(),test.toString());
+})
+.add('#zlib', function() {
+  var test = rb.zLibDecompress(rb.zLibCompress(buf))
+   assert.equal(buf.toString(),test.toString());
+   
+})// add listeners 
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async 
+.run({ 'async': true });
